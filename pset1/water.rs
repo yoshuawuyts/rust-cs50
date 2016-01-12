bb6f8127bbb54eed35470bc2630446d6466da9c3
Yoshua Wuyts <i@yoshuawuyts.com>
2016-01-13 02:50:03 +1100

    pset1: water

---
+++ pset1/water.rs
@@ -0,0 +1,21 @@
+// Write, in a file called water.c in your ~/workspace/pset1 directory, a
+// program that prompts the user for the length of his or her shower in minutes
+// (as a positive integer) and then prints the equivalent number of bottles of
+// water (as an integer) per the sample output below, wherein underlined text
+// represents some user’s input.
+//
+// username@ide50:~/workspace/pset1 $ ./water
+// minutes: 10
+// bottles: 120
+//
+// a 1-minute shower is akin to using 192 ÷ 16 = 12 bottles of water
+
+use std::io;
+use std::io::prelude::*;
+
+fn main () {
+  let stdin = io::stdin();
+  for line in stdin.lock().lines() {
+    println!("{}", line.unwrap());
+  }
+}
