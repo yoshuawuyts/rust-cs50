fecdae2a598234547469dc894a4cdecca7a969de
Yoshua Wuyts <i@yoshuawuyts.com>
2016-01-13 22:51:19 +1100

    pset1/water-safe: add

---
+++ pset1/water-safe.rs
@@ -0,0 +1,40 @@
+// Write, in a file called water.c in your ~/workspace/pset1 directory, a
+// program that prompts the user for the length of his or her shower in minutes
+// (as a positive integer) and then prints the equivalent number of bottles of
+// water (as an integer) per the sample output below, wherein underlined text
+// represents some user’s input.
+//
+// username@ide50:~/workspace/pset1 $ ./water
+// minutes: 10
+// bottles: 120
+//
+// a 1-minute shower is akin to using 192 ÷ 16 = 12 bottles of water
+//
+// http://stackoverflow.com/questions/28528998/how-do-i-read-a-single-string-from-standard-input
+
+use std::io;
+use std::io::prelude::*;
+
+fn main () {
+  let mut done = false;
+  while !done {
+    print!("minutes: ");
+    io::stdout().flush().unwrap();
+
+    let mut line = String::new();
+    let stdin = io::stdin();
+    stdin.lock().read_line(&mut line).unwrap();
+
+    match line.trim().parse::<i32>() {
+      Err(_) => println!("Incorrect input type, try again"),
+      Ok(n) => {
+        done = true;
+        print(n * 12)
+      },
+    }
+  }
+
+  fn print (num: i32) {
+    println!("bottles: {}", num);
+  }
+}
